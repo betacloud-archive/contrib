fdbd21ff5e35f5522cc42a7261fcb4c1b020a950
Christian Berendt <berendt@betacloud-solutions.de>
2018-03-06 13:03:50 +0100

    Add script to manage endpoint groups with project metadata

---
+++ keystone/metadata-assign-endpoint-group-to-project.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import sys
+
+import keystoneauth1
+import os_client_config
+import shade
+
+CLOUDNAME = 'service'
+
+BASE_ENDPOINT_GROUPS = {
+    "has_nova": ["nova", "nova_legacy"],
+    "has_heat": ["heat", "heat-cfn"],
+    "has_keystone": ["keystone"],
+    "has_cinder": ["cinder", "cinderv2", "cinderv3"],
+    "has_neutron": ["neutron"],
+    "has_glance": ["glance"],
+    "has_mistral": ["mistral"]
+}
+
+cloud = shade.operator_cloud(cloud=CLOUDNAME)
+keystone = os_client_config.make_client('identity', cloud=CLOUDNAME)
+
+try:
+    project = keystone.projects.get(project=os.environ.get("PROJECT"))
+except keystoneauth1.exceptions.http.NotFound:
+    print("project %s not found" % os.environ.get("PROJECT"))
+    sys.exit(1)
+
+existing_endpoint_groups = {x.name: x for x in keystone.endpoint_groups.list()}
+
+for group in [x for x in project.to_dict() if x.startswith("has_") and x in BASE_ENDPOINT_GROUPS]:
+    if project.to_dict()[group] in ["True", "true", "Yes", "yes"]:
+        for endpoint in BASE_ENDPOINT_GROUPS[group]:
+            print("assign endpoint group %s to project %s" % (endpoint, os.environ.get("PROJECT")))
+            try:
+                keystone.endpoint_filter.add_endpoint_group_to_project(
+                    endpoint_group=existing_endpoint_groups[endpoint].id,
+                    project=os.environ.get("PROJECT")
+                )
+            except keystoneauth1.exceptions.http.Conflict:
+                pass
+    elif project.to_dict()[group] in ["False", "false", "No", "no"]:
+        for endpoint in BASE_ENDPOINT_GROUPS[group]:
+            print("unassign endpoint group %s from project %s" % (endpoint, os.environ.get("PROJECT")))
+            try:
+                keystone.endpoint_filter.delete_endpoint_group_to_project(
+                    endpoint_group=existing_endpoint_groups[endpoint].id,
+                    project=os.environ.get("PROJECT")
+                )
+            except keystoneauth1.exceptions.http.NotFound:
+                pass
+    else:
+        print("project %s has wrong value %s for %s" % (os.environ.get("PROJECT"), project.to_dict()[group], group))
