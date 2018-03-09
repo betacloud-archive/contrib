8407ecba2df3684cee24a31b96fb447d5cea017c
Christian Berendt <berendt@betacloud-solutions.de>
2018-03-09 11:09:25 +0100

    Fix copy & paste issues

--- generic/expire-project-resources.py
+++ generic/expire-project-resources.py
@@ -38,12 +38,12 @@ MAILGUNKEY = os.environ.get("MAILGUNKEY", None)
 MAILGUNAPI = os.environ.get("MAILGUNAPI", "https://api.mailgun.net/v3/betacloud.io/messages")
 MAILGUNFROM = os.environ.get("MAILGUNFROM", "Betacloud Sag Wagon <noreply@betacloud.io>")
 
-DELETION_TEMPLATE = os.environ.get("EXPIRATION_TEMPLATE", "etc/expiration-deletion.yml.j2")
+DELETION_TEMPLATE = os.environ.get("DELETION_TEMPLATE", "etc/expiration-deletion.yml.j2")
 EXPIRATION_TEMPLATE = os.environ.get("EXPIRATION_TEMPLATE", "etc/expiration-reminder.yml.j2")
-EXPIRATION_TIME = timedelta(hours=os.environ.get("NEXT_EXPIRATION_TIME", 72))
-MAX_EXPIRATION_TIME = timedelta(hours=os.environ.get("NEXT_EXPIRATION_TIME", 288))
+EXPIRATION_TIME = timedelta(hours=os.environ.get("EXPIRATION_TIME", 72))
+MAX_EXPIRATION_TIME = timedelta(hours=os.environ.get("MAX_EXPIRATION_TIME", 144))
 
-REMINDER_TIME = timedelta(hours=os.environ.get("NEXT_EXPIRATION_TIME", 24))
+REMINDER_TIME = timedelta(hours=os.environ.get("REMINDER_TIME", 24))
 NEXT_EXPIRATION_TIME = timedelta(hours=os.environ.get("NEXT_EXPIRATION_TIME", 24))
 
 
